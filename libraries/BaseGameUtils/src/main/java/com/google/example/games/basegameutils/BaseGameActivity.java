9e2c94f3c6dbd36c65dd37427e77e1391b25c25c
joaobiriba <glaquidara@gmail.com>
2014-10-20 19:15:35 +0200

    Modified Style and adapting to API 21

--- libraries/BaseGameUtils/src/main/java/com/google/example/games/basegameutils/BaseGameActivity.java
+++ libraries/BaseGameUtils/src/main/java/com/google/example/games/basegameutils/BaseGameActivity.java
@@ -19,6 +19,7 @@ package com.google.example.games.basegameutils;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -40,7 +41,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
  *
  * @author Bruno Oliveira (Google)
  */
-public abstract class BaseGameActivity extends FragmentActivity implements
+public abstract class BaseGameActivity extends ActionBarActivity implements
         GameHelper.GameHelperListener {
 
     // The game helper object. This class is mainly a wrapper around this object.
